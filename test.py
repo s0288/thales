08214b0a1e7df30f33f24d0f5d968cba124f3cac
s0288 <alex.gansmann@gmail.com>
2019-10-29 22:56:28 +0100

    define next steps

--- test.py
+++ test.py
@@ -2,13 +2,18 @@ import pandas_datareader.data as web
 import numpy as np
 import pandas as pd
 
+####
+# Necessary adjustments:
+# - add points of time for investment (have one col as 'total_funds' and one as 'added_funds') and calculate 'weights' as result of shifting 'total_funds'  
+# --> pf["date", "pf_abs", "pf_ret", "pf_ret30", "original_funds", "assets" ARRAY["name_stock1", "name_stock2", "name_stock3"], "weights" ARRAY["weight_stock1", "weight_stock2", "weight_stock3"]]
+
 # pick tickers
 stocks = ['IVV', 'DE']
 # pf weights
 weights = np.asarray([0.5,0.5])
 
 # retrieve data
-data = web.DataReader(stocks,data_source="yahoo",start='01/10/2019')['Adj Close']
+data = web.DataReader(stocks,data_source="yahoo",start='01/01/2019')['Adj Close']
 data.sort_index(inplace=True)
 # create pf value
 data["pf_abs"] = data.dot(weights)
